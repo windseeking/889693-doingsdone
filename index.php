e5c12732d6c30f93c7cfdadb43d829a1367d00d5
windseeking <windseeking2@gmail.com>
2018-11-17 18:12:21 +0300

    Сделала код покрасивее

--- index.php
+++ index.php
@@ -80,7 +80,7 @@ $show_complete_tasks = rand(0, 1);
 	                        <?php 
 	                        if ($show_complete_tasks == 1)
 	                        	{ echo "checked"; } ?>	
-	                      >
+	                        >
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
@@ -104,17 +104,17 @@ $show_complete_tasks = rand(0, 1);
                     <?php 
                     if ($show_complete_tasks == 1) {
                     	echo '<tr class="tasks__item task task--completed">
-												    	<td class="task__select">
-												        <label class="checkbox task__checkbox">
-												            <input class="checkbox__input visually-hidden" type="checkbox" checked>
-												            <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
-												        </label>
-												    	</td>
-												   		<td class="task__date">10.10.2018</td>
+						    	<td class="task__select">
+						        <label class="checkbox task__checkbox">
+						            <input class="checkbox__input visually-hidden" type="checkbox" checked>
+						            <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
+						        </label>
+						    	</td>
+						   		<td class="task__date">10.10.2018</td>
 
-												    	<td class="task__controls"></td>
-														</tr>';} 
-										?>
+						    	<td class="task__controls"></td>
+							</tr>';} 
+					?>
                 </table>
             </main>
         </div>
