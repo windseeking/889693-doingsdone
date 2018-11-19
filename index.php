34211357a2b573e492ad3077ef68f29bd60bc251
windseeking <windseeking2@gmail.com>
2018-11-19 10:47:40 +0300

    Исправлено: работа с условиями

--- index.php
+++ index.php
@@ -76,12 +76,7 @@ $show_complete_tasks = rand(0, 1);
 
                     <label class="checkbox">
                         <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
-                        <input class="checkbox__input visually-hidden show_completed" type="checkbox" 
-	                        <?php 
-    	                        if ($show_complete_tasks == 1) {
-    	                        	echo "checked"; 
-                                } 
-                            ?>	
+                        <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?php echo ($show_complete_tasks) ? "checked" : "" ;?>
                         >
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
@@ -103,22 +98,19 @@ $show_complete_tasks = rand(0, 1);
                         <td class="task__date"></td>
                     </tr>
                     
-                    <?php 
-                        if ($show_complete_tasks == 1) {
-                        	echo 
-                            '<tr class="tasks__item task task--completed">
-    					    	<td class="task__select">
+                    <?php if ($show_complete_tasks): ?>
+                    <tr class="tasks__item task task--completed">
+    					    		<td class="task__select">
     						        <label class="checkbox task__checkbox">
-    						            <input class="checkbox__input visually-hidden" type="checkbox" checked>
-    						            <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
+  						            <input class="checkbox__input visually-hidden" type="checkbox" checked>
+  						            <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
     						        </label>
-    					    	</td>
-    					   		<td class="task__date">10.10.2018</td>
+    					    		</td>
+    					   			<td class="task__date">10.10.2018</td>
 
-    					    	<td class="task__controls"></td>
-    						</tr>'
-                        ;} 
-					?>
+    					    		<td class="task__controls"></td>
+    								</tr>
+    								<?php endif; ?>
                 </table>
             </main>
         </div>
